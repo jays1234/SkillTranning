d54f3866a2ee6921f963f48712ce3581fbc30776
jay <jays.phirayu@gmail.com>
2015-07-22 12:12:56 +0700

    Create Class Detach

--- src/main/java/Hibernate/Hibernate05Detach.java
+++ src/main/java/Hibernate/Hibernate05Detach.java
@@ -1,10 +1,26 @@
 package Hibernate;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.classic.Session;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Created by phirayu on 22/7/2558.
  */
 public class Hibernate05Detach {
+    Logger logger = LoggerFactory.getLogger(Hibernate05Detach.class);
     public static void main(String[] args){
+        SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+        Session session = sessionFactory.openSession();
+
+
+        session.close();
 
     }
+
+
+
+
 }
