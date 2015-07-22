1c482f10c10c9548ee84518c15f3a76652164025
jay <jays.phirayu@gmail.com>
2015-07-22 12:16:05 +0700

    rebase project

--- src/main/java/Hibernate/Hibernate05Detach.java
+++ src/main/java/Hibernate/Hibernate05Detach.java
@@ -1,10 +1,25 @@
 package Hibernate;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Created by phirayu on 22/7/2558.
  */
 public class Hibernate05Detach {
+    Logger logger = LoggerFactory.getLogger(Hibernate05Detach.class);
     public static void main(String[] args){
+        SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+        Session session = sessionFactory.openSession();
+
+
+
+     session.close();
+    }
+    private void detachCriteria(Session session){
 
     }
 }
